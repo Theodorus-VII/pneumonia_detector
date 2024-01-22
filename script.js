f885a2761bb7fa6e60f68347846998756ba093f9
Theodorus-VII <tediberhanu777@gmail.com>
2024-01-22 17:28:18 +0300

    Fixed model-loader function call bug

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import * as tf from '@tensorflow/tfjs';
+import { loadGraphModel, loadLayersModel } from '@tensorflow/tfjs';
+
 MODEL_URL = './saved_models/model23/tfjs/model23/model.json'
 async function loadModel(MODEL_URL){
     const model = await tf.loadGraphModel(MODEL_URL);
@@ -5,7 +8,9 @@ async function loadModel(MODEL_URL){
     console.log(model);
     return model;
 }
-model = loadModel(MODEL_URL);
+
+model = await loadGraphModel(MODEL_URL);
+console.log('model loaded successfully');
 
 
 
